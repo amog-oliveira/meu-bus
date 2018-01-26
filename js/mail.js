d0c763c9538763a484ea4e6f9b3b6d733a04e28c
Pedro Silva <pedro.rls93@gmail.com>
2018-01-25 23:42:52 -0200

    video was added

--- js/mail.js
+++ js/mail.js
@@ -1,11 +1,7 @@
 $(function () {
     var a = $("#form");
-<<<<<<< HEAD
-    $("#form-response"), $(a).submit(function (e) {
-=======
     $("#form-response");
     $(a).submit(function (e) {
->>>>>>> dev
         e.preventDefault();
         var o = $(a).serialize();
         $.ajax({
@@ -13,11 +9,6 @@ $(function () {
             url: "../mail.php",
             data: o
         }).done(function (a) {
-<<<<<<< HEAD
-            swal("Yay !", "Seu email foi enviado :)", "success"), $("#name").val(""), $("#email").val(""), $("#subject").val(""), $("#message").val(""), location.reload()
-        }).fail(function () {
-            swal("Oops !", "Oops ! Algum erro ocorreu e sua mensagem não pode ser enviada :( ", "error")
-=======
             swal("Yay !", "Seu email foi enviado :)", "success");
             $("#name").val("");
             $("#email").val("");
@@ -26,7 +17,6 @@ $(function () {
             location.reload();
         }).fail(function () {
             swal("Oops !", "Oops ! Algum erro ocorreu e sua mensagem não pode ser enviada :( ", "error");
->>>>>>> dev
         })
     })
 });
